a91785ef130ae2da3b07fc98c0e423b986fbf9b9
Featos <46037898+Featos@users.noreply.github.com>
2019-03-22 20:02:14 +0700

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,9 @@
+//var sum = 0;
+//for (var i = 0; i < 1001; i++) { sum += i;}
+//document.write(sum);
+
+//for (var i = 1; i < 11; i++) {
+//    var rs = 2 * i;
+//    document.writeln("2" + " " + "*" + i + " " + "=" + " " + rs + "<br>");
+//}
+
